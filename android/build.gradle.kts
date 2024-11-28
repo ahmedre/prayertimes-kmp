f1fddc9a8100bb0a09cf5134e2ed3c50623050b7
Ahmed El-Helw <ahmedre@gmail.com>
2024-11-29 02:29:53 +0400

    Update Kotlin to 2.1.0 and update dependencies

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
   id("com.android.application")
   kotlin("android")
+  id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
 }
-
-val composeCompilerVersion = "1.5.10"
 
 android {
   namespace = "app.salah.android"
@@ -12,7 +11,7 @@ android {
   defaultConfig {
     applicationId = "app.salah.android"
     minSdk = 21
-    targetSdk = 34
+    targetSdk = 35
     versionCode = 1
     versionName = "1.0"
   }
@@ -24,10 +23,6 @@ android {
   }
 
   buildFeatures.compose = true
-
-  composeOptions {
-    kotlinCompilerExtensionVersion = composeCompilerVersion
-  }
 
   kotlinOptions {
     jvmTarget = "17"
@@ -47,13 +42,13 @@ kotlin {
 dependencies {
   implementation(project(":shared"))
   implementation(project(":renderer"))
-  implementation("androidx.appcompat:appcompat:1.6.1")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+  implementation("androidx.appcompat:appcompat:1.7.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 
   // jetpack compose
-  implementation(platform("androidx.compose:compose-bom:2024.02.01"))
-  implementation("androidx.activity:activity-compose:1.8.2")
+  implementation(platform("androidx.compose:compose-bom:2024.11.00"))
+  implementation("androidx.activity:activity-compose:1.9.3")
   implementation("androidx.compose.runtime:runtime")
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.foundation:foundation-layout")
